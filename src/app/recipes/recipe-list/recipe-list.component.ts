9056089b6c72cbe792dbe843d6cc8d5f41b82440
behrang <behrang1360@gmail.com>
2019-08-30 20:55:46 +0430

    add recipe item to the list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.Model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
+  Recipes: Recipe[] = [
+    new Recipe(
+      "A Test Recipe",
+      " Descripton goes hear",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJzP1gbxofQX0zV6XdWJrxJxYV0x9UtMqBZaCpfK58mr_sa5D8"
+    )
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  AddNewRecipe() {
+    this.Recipes.push(
+    new Recipe(
+      "A Test Recipe" + Math.random(),
+      " Descripton goes hear",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJzP1gbxofQX0zV6XdWJrxJxYV0x9UtMqBZaCpfK58mr_sa5D8n"
+    ));
   }
-
 }
